795bb66587d6124966d19f5b8dd5492548a9c2e1
leechanwoo <90599105+BugEliminator@users.noreply.github.com>
2025-07-07 11:30:03 +0900

    chore: Airbnb ESLint 테스트 코드 추가

--- src/app/test.tsx
+++ src/app/test.tsx
@@ -1,11 +1,13 @@
-// src/app/test.tsx
+import React from "react"; // 큰따옴표 위반
 
-import React from "react";
+var count = 0; // var 사용 금지
 
-function Test() {
-  // console.log("테스트입니다");
+function Example() {
+  console.log("테스트"); // 콘솔 금지
 
-  return <div>Hello World</div>;
+  const msg = "Hello " + "World"; // 템플릿 리터럴 권장
+
+  return <div>{msg}</div>;
 }
 
-export default Test;
+export default Example;

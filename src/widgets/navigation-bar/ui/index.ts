6334409a68bcf667e25c4295a0bb8a6a126156df
dropper-in <dropperin59@gmail.com>
2025-07-21 16:25:07 +0900

    refactor: 내비게이션 바 UI 컴포넌트 FSD 구조에 맞게 경로 변경

---
+++ src/widgets/navigation-bar/ui/index.ts
@@ -0,0 +1,4 @@
+export { default as LoginButton } from './LoginButton';
+export { default as NavigationTabs } from './NavigationTabs';
+export { default as UserDropdown } from './UserDropdown';
+export { default as UserIcon } from './UserIcon';

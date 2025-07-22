b9ed48914e9dd3ba1ad13b1ae28af4f3165aba3e
Lee Ye Seo <rosefor2ne@gmail.com>
2025-07-22 09:41:05 +0900

    refactor: MeetingList getApi 제네릭 타입 명시

--- src/entities/meetings/api/getMeetingList.ts
+++ src/entities/meetings/api/getMeetingList.ts
@@ -4,6 +4,6 @@ import { MeetingFilters, MeetingList } from '../model/types';
 export default async function getMeetingList(
   filter: MeetingFilters,
 ): Promise<MeetingList[] | null> {
-  const res = await getApi('/api/v1/meetings', { params: filter });
+  const res = await getApi<{ content: MeetingList[] }>('/api/v1/meetings', { params: filter });
   return res ? res.content : null;
 }

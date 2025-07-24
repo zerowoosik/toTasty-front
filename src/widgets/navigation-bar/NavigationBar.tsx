8f022d426dc3851b06ad403af804641b5c5947b0
Dropper-in <76846986+Dropper-in@users.noreply.github.com>
2025-07-24 10:24:02 +0900

    fix: 내비게이션 바 버그 수정 (#96)
    * fix: 내비게이션 바 버그 수정
    
    z-index 수정 및 컬러값 변경, useRouter import 경로 변경
    
    * style: 내비게이션 바 색상 클래스 수정
    
    ---------
    
    Co-authored-by: dropper-in <dropperin59@gmail.com>

--- src/widgets/navigation-bar/NavigationBar.tsx
+++ src/widgets/navigation-bar/NavigationBar.tsx
@@ -14,7 +14,7 @@ export default function NavigationBar() {
   );
   // const isLoggedIn = true;
   return (
-    <div className="w-full bg-white flex justify-center border-b border-[#E3E7EB] sticky top-0 z-50">
+    <div className="flex justify-center w-full sticky top-0 bg-white border-b border-gray-020 z-15">
       <div className="flex justify-between h-[75px] w-7xl items-center px-5">
         <NavigationMenu className="gap-6">
           <Logo />
